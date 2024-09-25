c96116b844644c6e10ffc82f76606448feefb269
Christian Cachero <Ch_canchero@live.com.ar>
2024-09-25 20:30:00 -0300

    Tercer commit, se arregló algunos errores, se anotaron con @Repository los respetivos repositorios y se inyectaron a la clase principal del proyecto, agrego la funcionalidad "Mostrar Libros" y "Mostrar Libros Por Idioma", y a esta última se le agregó una pequeña comprobación por si no existiera el idioma seleccionado. Se marcaron como EAGER algunas variables en las tablas para evitar un problema de lazy load, lo revisaré más tarde para ver si hay una mejor manera de hacer esto.

--- src/main/java/com/aluradevsafios/LiterAlura/repository/PersonaRepository.java
+++ src/main/java/com/aluradevsafios/LiterAlura/repository/PersonaRepository.java
@@ -2,9 +2,11 @@ package com.aluradevsafios.LiterAlura.repository;
 
 import com.aluradevsafios.LiterAlura.models.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
    Optional<Persona> findByNombre(String nombre);
