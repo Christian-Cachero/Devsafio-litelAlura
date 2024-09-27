a1a167ed8b4554da6a3be9078233676b6f9c4bfb
Christian Cachero <Ch_canchero@live.com.ar>
2024-09-27 01:12:35 -0300

    4to commit, se agregaron 3 nuevas funcionalidades (Listar autores de los libros buscados/Listar autores vivos (rango de años)/Listar cantidad de libros en español o inglés)
    y con esto se alcanzaron todos los requerimientos del proyecto/devsafio LiterAlura.
    
    ahora solo queda agregar algunas comprobaciones y darlo por terminado.

--- src/main/java/com/aluradevsafios/LiterAlura/repository/PersonaRepository.java
+++ src/main/java/com/aluradevsafios/LiterAlura/repository/PersonaRepository.java
@@ -4,10 +4,14 @@ import com.aluradevsafios.LiterAlura.models.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
    Optional<Persona> findByNombre(String nombre);
+
+   Optional<List<Persona>> findByFechaMuerteGreaterThanEqualAndFechaNacimientoLessThanEqual(Integer periodoNacido, Integer periodoMuerte);
+   //Optional<Persona> findByFechaMuerte();
 }
